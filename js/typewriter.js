006c497dbab60474932bf7d03b0e84a4d04bf1d9
manny sandhu <manny.sandhu.1992@gmail.com>
2025-01-29 17:08:32 +0000

    Updated index and typewriter.

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,57 +1,66 @@
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 1000;
+    this.txt = '';
+    this.isDeleting = false;
+    this.tick();
+};
 
-		var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 1000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
+    if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
+    } else {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+    }
 
-        this.el.innerHTML = '> <span class="wrap">'+this.txt+'</span>';
+    // Static arrow + typed text
+    this.el.innerHTML = 
+      '<span style="font-size: 0.9rem; font-family: Fira Code, monospace;">>&nbsp;</span>' +
+      '<span class="wrap" style="font-size: 0.9rem; font-family: Fira Code, monospace;">' +
+      this.txt +
+      '</span>';
 
-        var that = this;
-        var delta = 130 - Math.random() * 100;
+    var that = this;
+    var delta = 130 - Math.random() * 100;
 
-        if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+        delta /= 2;
+    }
 
-        if (!this.isDeleting && this.txt === fullTxt) {
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
+    } 
+    // If word is fully deleted
+    else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
         delta = 100;
-        }
+    }
 
-        setTimeout(function() {
+    setTimeout(function () {
         that.tick();
-        }, delta);
-    };
+    }, delta);
+};
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.05em solid #fff}";
-        document.body.appendChild(css);
-    };
+    }
+    // Optional blinking cursor style
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite .wrap { border-right: 0.05em solid #fff }";
+    document.body.appendChild(css);
+};
